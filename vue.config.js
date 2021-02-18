8ad93f8a98ac5585cb9aee1aa63563513f46486f
Artyom Lebedev <vagran.ast@gmail.com>
2021-02-18 21:31:00 +0200

    Proper npm dependency.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? "/dxf-viewer-example/"
-        : "/"
+        : "/",
+    transpileDependencies: [
+        /[\\\/]node_modules[\\\/]dxf-viewer[\\\/]/
+    ]
 }
