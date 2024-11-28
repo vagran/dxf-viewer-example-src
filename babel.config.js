a8171104e6091c06fbc42d13b0493351e042e1d7
Artyom Lebedev <vagran.ast@gmail.com>
2024-11-28 21:03:01 +0200

    Adopt latest dxf-viewer.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     "plugins": ["@babel/plugin-proposal-class-properties",
                 "@babel/plugin-transform-nullish-coalescing-operator",
-                "@babel/plugin-transform-optional-chaining"]
+                "@babel/plugin-transform-optional-chaining",
+                "@babel/plugin-transform-logical-assignment-operators"]
 }
